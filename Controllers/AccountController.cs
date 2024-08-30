e36e33a5e52c0c6944cdbfc5e10926ba2ba252d3
Ishita25-sys <ishu.252502@gmail.com>
2024-08-30 14:55:08 +0530

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SoundWaveOfficial.Models;
+
+namespace SoundWaveOfficial.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Membership model)
+        {
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(tbl_Registration1 model)
+        {
+            using(var context =new SoundWaveOfficialEntities1())
+            {
+                context.tbl_Registration1.Add(model);
+                context.SaveChanges();
+
+            }
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Controllers/tbl_PodcastController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoundWaveOfficial.Controllers
+{
+    public class tbl_PodcastController : Controller
+    {
+        // GET: tbl_Podcast
+        SqlConnection conn = new SqlConnection("Data Source=LAPTOP-VAN8158V\\SERVER11;Initial Catalog=SoundWaveOfficial;Persist Security Info=True;User ID=sa;Password=Server11;Application Name=EntityFramework");
+        List<tbl_Podcast> podcastList = new List<tbl_Podcast>();
+        public ActionResult Index()
+        {
+            string qry = "select * from tbl_Podcast";
+            SqlCommand cmd=new SqlCommand(qry, conn);
+            conn.Open();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+            podcastList.Add(new tbl_Podcast
+            {
+                PodcastId = Convert.ToInt32(dr["PodcastId"]),
+                PodcastName = dr["PodcastName"].ToString(),
+                AuthorName = dr["AuthorName"].ToString(),
+                Category = dr["Category"].ToString(),
+                UserId = Convert.ToInt32(dr["UserId"]),
+                UploadFile = dr["UploadFile"].ToString()
+            });
+            return View(podcastList);
+        }
+    }
+}
